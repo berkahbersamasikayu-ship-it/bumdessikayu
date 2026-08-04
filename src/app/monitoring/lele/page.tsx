cc50b42c434e9233319799b7b44b593072c381e9
radityankw <radityantari41@gmail.com>
2026-08-05 00:51:25 +0700

    revisi

--- src/app/monitoring/lele/page.tsx
+++ src/app/monitoring/lele/page.tsx
@@ -95,7 +95,7 @@ export default function MonitoringLelePage() {
           <option value="kualitas">Monitoring Kualitas Budidaya</option>
         </select>
 
-        <div className="flex flex-col w-full md:w-auto items-center gap-3 justify-between">
+        <div className="flex w-full md:w-auto items-center gap-3">
           <div className="relative w-full md:w-auto">
             <Search className="w-4 h-4 text-gray-400 absolute left-3 top-1/2 -translate-y-1/2" />
             <input
@@ -107,27 +107,29 @@ export default function MonitoringLelePage() {
             />
           </div>
 
-          <button
-            onClick={() => handleExport('excel')}
-            className="flex items-center gap-2 px-4 py-2.5 rounded-lg bg-green-600 text-white text-sm font-semibold hover:bg-green-700"
-          >
-            <FileSpreadsheet size={16} /> Export Excel
-          </button>
-          <button
-            onClick={() => handleExport('pdf')}
-            className="flex items-center gap-2 px-4 py-2.5 rounded-lg bg-blue-900 text-white text-sm font-semibold hover:bg-blue-800"
-          >
-            <FileText size={16} /> Ecport PDF
-          </button>
-
-          {!isViewer && (
+          <div className="flex flex-col md:flex-row items-start md:items-center gap-3 w-full md:w-auto">
             <button
-              onClick={() => setShowModal(true)}
-              className="h-11 px-6 rounded-lg bg-green-700 text-white font-semibold text-sm hover:bg-green-800 transition-colors shrink-0"
+              onClick={() => handleExport('excel')}
+              className="flex items-center gap-2 px-4 py-2.5 rounded-lg bg-green-600 text-white text-sm font-semibold hover:bg-green-700"
             >
-              Tambah Data
+              <FileSpreadsheet size={16} /> Export Excel
             </button>
-          )}
+            <button
+              onClick={() => handleExport('pdf')}
+              className="flex items-center gap-2 px-4 py-2.5 rounded-lg bg-blue-900 text-white text-sm font-semibold hover:bg-blue-800"
+            >
+              <FileText size={16} /> Ecport PDF
+            </button>
+
+            {!isViewer && (
+              <button
+                onClick={() => setShowModal(true)}
+                className="h-11 px-6 rounded-lg bg-green-700 text-white font-semibold text-sm hover:bg-green-800 transition-colors shrink-0"
+              >
+                Tambah Data
+              </button>
+            )}
+          </div>
         </div>
       </div>
 
